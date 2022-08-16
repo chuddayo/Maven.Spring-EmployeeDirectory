f135243c7441cde306cf9b683095025211599bf5
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-16 18:21:40 -0400

    comments for API points so far

--- src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
+++ src/main/java/io/zipcoder/persistenceapp/controllers/EmployeeController.java
@@ -15,52 +15,61 @@ public class EmployeeController {
         employeeService = service;
     }
 
+    // CREATE an employee
     @PostMapping("/employee")
     public ResponseEntity<Employee> create(@RequestBody Employee employee) {
         return new ResponseEntity<>(employeeService.create(employee), HttpStatus.CREATED);
     }
 
+    // UPDATE employee with certain {id} to have new {managerID}
     @PutMapping("/employee/{id}/newmanager/{managerID}")
     public ResponseEntity<Employee> updateManager(@PathVariable Integer id, @PathVariable Integer managerID) {
         return new ResponseEntity<>(employeeService.updateManager(id, managerID), HttpStatus.OK);
     }
 
+    // UPDATE employee with certain {id}
     @PutMapping("/employee/{id}")
     public ResponseEntity<Employee> update(@PathVariable Integer id, @RequestBody Employee employee) {
         return new ResponseEntity<>(employeeService.update(id, employee), HttpStatus.OK);
     }
 
+    // GET all employees as Iterable
     @GetMapping("/employee")
     public ResponseEntity<Iterable<Employee>> index() {
         return new ResponseEntity<>(employeeService.index(), HttpStatus.OK);
     }
 
+    // GET employee with certain {id}
     @GetMapping("/employee/{id}")
     public ResponseEntity<Employee> show(@PathVariable Integer id) {
         return new ResponseEntity<>(employeeService.show(id), HttpStatus.OK);
     }
 
+    // GET all employees who report directly to a particular manager with {id}
     @GetMapping("/employee/{id}/directreports")
     public ResponseEntity<List<Employee>> getDirectReports(@PathVariable Integer id) {
         return new ResponseEntity<>(employeeService.getDirectReports(id), HttpStatus.OK);
     }
 
-    // Get all employees who report directly or indirectly to a particular manager
+    // GET all employees who report directly or indirectly to a particular manager with {id}
     @GetMapping("/employee/{id}/allreports")
     public ResponseEntity<List<Employee>> getAllReports(@PathVariable Integer id) {
         return new ResponseEntity<>(employeeService.getAllReports(id), HttpStatus.OK);
     }
 
+    // GET reporting hierarchy upwards from with employee with {id}
     @GetMapping("/employee/{id}/hierarchy")
     public ResponseEntity<List<Employee>> getReportingHierarchy(@PathVariable Integer id) {
         return new ResponseEntity<>(employeeService.getReportingHierarchy(id), HttpStatus.OK);
     }
 
+    // GET all employees who do not report to a manager
     @GetMapping("/employee/bosses")
     public ResponseEntity<List<Employee>> getBosses() {
         return new ResponseEntity<>(employeeService.getBosses(), HttpStatus.OK);
     }
 
+    // GET all employees who work in department with departmentID {id}
     @GetMapping("/employee/department/{id}")
     public ResponseEntity<List<Employee>> getDepartmentEmployees(@PathVariable Integer id) {
         return new ResponseEntity<>(employeeService.getDepartmentEmployees(id), HttpStatus.OK);
